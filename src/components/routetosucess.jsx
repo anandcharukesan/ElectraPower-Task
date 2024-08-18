286d526b829d4d719711fe5b6e75acd8b54a4652
anandcharukesan <anandcharukesan004@gmail.com>
2024-08-18 14:19:07 +0530

    img path modify

--- src/components/routetosucess.jsx
+++ src/components/routetosucess.jsx
@@ -13,12 +13,12 @@ const RouteToSuccess = () => {
                 </h2>
             </div>
             <div className='tubeContainer'>
-<Tube name="Expertise" hiddenImage="..\src\assets\Images\1.png" number="1" left="0px" width="400px"/>
-<Tube name="Agility" hiddenImage="..\src\assets\Images\2.png" number="2" left="150px" width="400px"/>
-<Tube name="Integrity" hiddenImage="..\src\assets\Images\3.png" number="3" left="330px" width="400px"/>
-<Tube current="true" name="Innovative" hiddenImage="..\src\assets\Images\4.png" number="4" left="520px" width="400px"/>
-<Tube name="Client Centric" hiddenImage="..\src\assets\Images\5.png" number="5" left="760px" width="400px"/>
-<Tube name="Collaboration" hiddenImage="..\src\assets\Images\6.png" number="6" left="1000px" width="300px"/>
+                <Tube name="Expertise" hiddenImage="./1.png" number="1" left="0px" width="400px" />
+                <Tube name="Agility" hiddenImage="./2.png" number="2" left="150px" width="400px" />
+                <Tube name="Integrity" hiddenImage="./3.png" number="3" left="330px" width="400px" />
+                <Tube current="true" name="Innovative" hiddenImage="./4.png" number="4" left="520px" width="400px" />
+                <Tube name="Client Centric" hiddenImage="./5.png" number="5" left="760px" width="400px" />
+                <Tube name="Collaboration" hiddenImage="./6.png" number="6" left="1000px" width="300px" />
             </div>
         </div>
     );
