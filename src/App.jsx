f1a48e2f28151704ec35c1637ea7a5d041c19baa
anandcharukesan <anandcharukesan004@gmail.com>
2024-08-18 15:07:08 +0530

    font updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import RouteToSuccess from './components/routetosucess'
 import Header from './components/Header'
 import ImgSlider from './components/ImgSliderText'
 import ImageFlow from './components/ImageFlow'
+import 'avenir';
+
 
 function App() {
 
